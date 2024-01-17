ecea10e089c9ca1173b55bc47c1ac6d6efa61793
Jon Love <124639155+jonnny013@users.noreply.github.com>
2024-01-17 17:43:59 +0800

    Fix navbar with material ui tabs

--- src/components/Header/NavBar.tsx
+++ src/components/Header/NavBar.tsx
@@ -1,12 +1,10 @@
+import NavTabs from "./NavTabs";
 import SocialIcons from "./SocialIcons";
-import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return <div id='header-link'>
     <SocialIcons />
-    <Link to='/'>Home</Link>
-    <Link to='contact'>Contact</Link>
-    <Link to='about'>About Me</Link>
+    <NavTabs />
   </div>;
 }
 
---
+++ src/components/Header/NavTabs.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+import Tabs from '@mui/material/Tabs'
+import Tab from '@mui/material/Tab'
+import { Link, useLocation } from 'react-router-dom'
+
+interface LinkTabProps {
+  label?: string
+  to?: string
+  selected?: boolean
+}
+
+function LinkTab(props: LinkTabProps) {
+  return (
+    <Tab
+      component={Link}
+      to={props.to || '/'}
+      aria-current={props.selected && 'page'}
+      {...props}
+    />
+  )
+}
+
+const NavTabs = () => {
+  const location = useLocation()
+  const [value, setValue] = React.useState(0)
+
+  React.useEffect(() => {
+    const paths = ['/', '/about', '/contact']
+    const currentIndex = paths.findIndex(path => path === location.pathname)
+    setValue(currentIndex !== -1 ? currentIndex : 0)
+  }, [location.pathname])
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue)
+  }
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        aria-label='nav tabs example'
+        role='navigation'
+      >
+        <LinkTab label='Projects' to='/' selected={value === 0} />
+        <LinkTab label='About Me' to='/about' selected={value === 1} />
+        <LinkTab label='Contact' to='/contact' selected={value === 2} />
+      </Tabs>
+    </Box>
+  )
+}
+
+export default NavTabs
