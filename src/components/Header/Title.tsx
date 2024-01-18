c02c1f9401ffde68057024d17c3b4c609a308ad1
Jon Love <124639155+jonnny013@users.noreply.github.com>
2024-01-18 14:25:22 +0800

    Fix title css and styles

--- src/components/Header/Title.tsx
+++ src/components/Header/Title.tsx
@@ -1,14 +1,17 @@
-
 const Title = () => {
   return (
-    <div>
-      <img className='logo' src='../../../mainLogoCropped.png' alt='logo' />
-      <h1 className='header'>Jon Love's Portfolio</h1>
-      <p className='header' id='pageDescription'>
-        This website is dedicated to putting my projects in one place
-      </p>
+    <div id='titleDiv' style={{display: 'flex'}}>
+      <div className='logoContainer' >
+        <img className='logo' src='../../../mainLogoCropped.png' alt='logo' />
+      </div>
+      <div style={{justifySelf: 'center', margin: 'auto', textAlign: 'center', padding: 10}}>
+        <h1 className='header'>Jon Love's Portfolio</h1>
+        <p className='header' id='pageDescription'>
+          This website is dedicated to putting my projects in one place
+        </p>
+      </div>
     </div>
-  );
+  )
 }
 
 export default Title
