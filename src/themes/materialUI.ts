cf4e7e45dd688b76a2e93d79f3fdd19b7cf334a8
Jon Love <124639155+jonnny013@users.noreply.github.com>
2024-01-22 20:11:28 +0800

    update MUI pallette

--- src/themes/materialUI.ts
+++ src/themes/materialUI.ts
@@ -3,11 +3,17 @@ import createTheme from '@mui/material/styles/createTheme'
 export const materialUIThemeDark = createTheme({
   palette: {
     mode: 'dark',
+    background: {
+      default: '#1c1c3f',
+    },
   },
 })
 
 export const materialUIThemeLight = createTheme({
   palette: {
     mode: 'light',
+    background: {
+      default: '#fff',
+    },
   },
 })
