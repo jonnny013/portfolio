906cec6933659cad1f37066459d6c6968fa6c08b
Jon Love <124639155+jonnny013@users.noreply.github.com>
2024-01-23 11:06:53 +0800

    add aboutMeServices

---
+++ src/services/aboutMeServices.ts
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+const baseURL = 'http://localhost:3001/aboutMe'
+
+export const getAboutMe = async () => {
+  const result = await axios.get(baseURL)
+  return result.data
+}
+
+export const addAboutMe = async (info) => {
+  const result = await axios.post(baseURL, info)
+  return result.data
+}
+
+export const updateAboutMe = async info => {
+  const result = await axios.put(`${baseURL}/${info.id}`, info)
+  return result.data
+}
+
+export const deleteAboutMe = async info => {
+  const result = await axios.delete(`${baseURL}/${info.id}`, info)
+  return result.data
+}
