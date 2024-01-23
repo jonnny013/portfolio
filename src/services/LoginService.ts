a30586fb84e8a99924c85d49369a3c5aa10db3df
Jon Love <124639155+jonnny013@users.noreply.github.com>
2024-01-23 10:33:31 +0800

    Adjust login service

--- src/services/LoginService.ts
+++ src/services/LoginService.ts
@@ -1,8 +1,13 @@
 import type { LoginFormTypes } from '../types'
+import axios from 'axios'
 
-const loginPost = ({ username, password}: LoginFormTypes) => {
-  console.log('Form to send', username, password)
-  return 'Sent!'
+const baseURL = 'http://localhost:3001/login'
+
+const loginPost = async ({ username, password}: LoginFormTypes) => {
+  const user = {username, password}
+  const result = await axios.post(baseURL, user)
+  console.log('Form to send', user)
+  return result.data
 }
 
 export default loginPost
