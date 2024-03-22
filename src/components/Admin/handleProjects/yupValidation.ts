edcea8e19b599e8e8bb0f0d9b38b335433cc8310
Jon Love <124639155+jonnny013@users.noreply.github.com>
2024-03-22 14:22:50 +0800

    update validation

--- src/components/Admin/handleProjects/yupValidation.ts
+++ src/components/Admin/handleProjects/yupValidation.ts
@@ -15,17 +15,11 @@ import * as yup from 'yup'
      .required('Description is required'),
    website: yup
      .string()
-     .matches(
-       /^(https?:\/\/)?([a-zA-Z0-9_-]+\.){1,}[a-zA-Z]{2,}(:[0-9]+)?(\/[\w#]+)*\/?(\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?$/,
-       'URL is not valid'
-     )
+     .url()
      .required('Please enter a valid website URL'),
    sourceCode: yup
      .string()
-     .matches(
-       /^(https?:\/\/)?([a-zA-Z0-9_-]+\.){1,}[a-zA-Z]{2,}(:[0-9]+)?(\/[\w#]+)*\/?(\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?$/,
-       'URL is not valid'
-     ),
+     .url(),
    skills: yup.object().test({
      name: 'areSkillsSelected',
      message: 'At least one skill must be selected',
