ce9e472af2d0934dc93bbbc8d09f6f1e6ee83b39
Jon Love <jon_w-l@hotmail.com>
2024-11-15 16:09:40 +0800

    Add future flags to router

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
     <UserProvider>
       <DarkModeProvider>
-        <Router>
+        <Router future={{ v7_relativeSplatPath: true, v7_startTransition: true }}>
           <App />
         </Router>
       </DarkModeProvider>
