1ebb52ea6a207fdb24b730e4908e376785a942c1
Jon Love <jon_w-l@hotmail.com>
2024-11-30 14:00:22 +0800

    Remove Error component

--- src/components/Error.tsx
+++
@@ -1,7 +0,0 @@
-import Alert from '@mui/material/Alert'
-
-const Error = () => {
-  return <Alert severity='error' sx={{fontSize: 20, textAlign:'center'}}>Sorry!! Something has gone wrong.</Alert>
-}
-
-export default Error
